d2c93f91ea623e396496352560f61f114b7ce458
bencheikhmakerem <makerem.bencheikh@gmail.com>
2022-02-15 22:27:08 +0100

    Initial commit

---
+++ tp-teams-handling_api/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO team (name, slogan) VALUES ('Tunisie', 'Les aigles de carthage');
+INSERT INTO team (name, slogan) VALUES ('Real Madrid', 'Les galactiques');
+INSERT INTO team (name, slogan) VALUES ('Barcelone', 'La Macia');
+INSERT INTO team (name, slogan) VALUES ('Bayern', 'Les puissants en Allemagne');
+INSERT INTO team (name, slogan) VALUES ('Manchester United', 'Les red devils');
+
+
+insert into Player (id, name, number, position,team_id) values(1,'Youssef Msekni','28','Ailier gauche',1);
+insert into Player (id, name, number, position,team_id) values(2,'Hannibal Mjebri','10','Milieu de terrain',2);
+insert into Player (id, name, number, position,team_id) values(3,'Cristiano Ronaldo','7','Attaquant',3);
+insert into Player (id, name, number, position,team_id) values(4,'Kylian Mbappé','10','Attaquant',4);
+
